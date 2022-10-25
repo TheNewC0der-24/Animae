70ed57462461233207e11237d8910f233162080c
Yabhaay <abhay.y2001@gmail.com>
2022-10-25 20:01:36 +0530

    created header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="navbar-brand mx-auto " to="/" style={{cursor:"pointer"}}>
+            THE <span className="fw-bolder animae-font"> ANIMAE</span> DATABASE
+          </div>
+        </div>
+      </nav>
+
+    </>
+  );
+};
+
+export default Header;
