3bcf801d6bdae95f984a0103ef8de05fc0746181
Yabhaay <abhay.y2001@gmail.com>
2022-10-25 20:01:50 +0530

    created leftsidebar

---
+++ src/components/leftSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopAnimaeList from './topAnimaeList'
+
+const LeftSidebar = () => {
+  return (
+      <div className='float-start mx-3 my-5'>
+          <TopAnimaeList/>
+    </div>
+  )
+}
+
+export default LeftSidebar
