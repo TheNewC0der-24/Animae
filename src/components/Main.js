f622e97320f7337f557f86782787e79837fd2169
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-27 01:12:51 +0530

    Change Main Layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import Header from './header'
+import React, { useState, useEffect } from 'react';
+
+import Header from './header';
 import LeftSidebar from './leftSidebar';
 import Search from "./searchBar";
 import Results from './results';
+import axios from 'axios';
 
 const Main = () => {
 
@@ -21,17 +23,42 @@ const Main = () => {
     getTopAnime();
   }, []);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchAnime(searchAnime);
+  }
+
+  const fetchAnime = async (query) => {
+    const temp = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}&order_by=title&sort=asc&limit=20`);
+    const data = temp.data.data.map((item) => {
+      return {
+        img: item.images.jpg.large_image_url,
+        title: item.title_english,
+        body: item.synopsis,
+        url: item.url,
+      };
+    })
+    setAnimeData(data);
+  }
+
   return (
     <>
       <Header />
       <div className='mx-5 mt-5'>
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-md-3 bg-light h-50 p-3 mb-5">
             <LeftSidebar topAnime={topAnime} />
           </div>
           <div className="col-md-9">
-            <Search />
-            <Results />
+            <Search
+              handleSearch={handleSearch}
+              searchAnime={searchAnime}
+              setSearchAnime={setSearchAnime}
+            />
+            <hr />
+            <Results
+              animeData={animeData}
+            />
           </div>
         </div>
       </div>
