02543e00386c4c2623724a018ee079a20a3cd503
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-27 01:13:16 +0530

    ✨ feat: Add Search Functionality

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
   return (
     <>
       <div className="d-grid col-6 ms-auto">
-        <input type="text" className="form-control mb-5" placeholder="Search for an anime..." />
+        <form className="d-flex mb-2" onSubmit={props.handleSearch}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for an anime..."
+            value={props.searchAnime}
+            onChange={(e) => props.setSearchAnime(e.target.value)}
+          />
+          <button disabled={props.searchAnime ? false : true} className="btn btn-primary search-button">Search</button>
+        </form>
       </div>
     </>
   );
