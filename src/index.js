07c068e80d88f2f16ef0d241504ff1c8ec9e8a8a
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-27 01:05:19 +0530

    Remove unnecessary import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/css/main.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
