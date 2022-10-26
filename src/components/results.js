9951dd0b45eadae83e537d8433e186bec827d752
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-27 01:54:11 +0530

    ✨ feat: Fix UI and Fetch the Data Again

--- src/components/results.js
+++ src/components/results.js
@@ -1,85 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import dummy from "../assets/images/dummy.png";
+import TextTruncate from 'react-text-truncate';
 
-import axios from "axios";
+import dummy from "../assets/dummy.png";
+import defaultImg from "../assets/default.png";
 
-const Results = () => {
-  const [data, setData] = useState([]);
-
-  const url = "https://api.jikan.moe/v4/anime";
-
-  useEffect(() => {
-    const getAnimaeData = async () => {
-      var apiData = await axios.get(url)
-      var passData = apiData.data.data.map((item) => {
-        return {
-          img: item.trailer.images.image_url,
-          title: item.title_english,
-          body: item.synopsis,
-          url: item.url,
-        };
-      })
-      setData(passData)
-    }
-    getAnimaeData();
-  }, [])
+const Results = (props) => {
 
   return (
     <>
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        <div className="col">
-          <div className="card">
-            <img
-              src={dummy}
-              className="card-img-top img-fluid img-thumbnail"
-              alt="Image Not Found"
-            ></img>
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                Watch
-              </a>
+        {props.animeData.length > 0 ?
+          props.animeData.map(anime => (
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={anime.img ? anime.img : dummy}
+                  className="img-fluid img-thumbnail"
+                  alt="thumbnail"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{anime.title ? anime.title : "Anonymous"}</h5>
+                  <span className="badge bg-primary">{anime.rating}</span>
+                  <hr />
+                  <TextTruncate line={3} element="p" truncateText="…" text={anime.body} className="card-text" />
+                  <a href={anime.url} target="_blank" rel="noreferrer" className="btn btn-primary d-block">
+                    Read More
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="card">
-            <img
-              src=""
-              className="card-img-top"
-              alt="Image Not Found"
-            ></img>
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                Watch
-              </a>
+          )) : (
+            <div className="w-100">
+              <img className="img-fluid d-flex mx-auto" width={300} src={defaultImg} alt="No Record" />
+              <h1 className="text-center">Sorry We have Nothing to show :(</h1>
             </div>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="card">
-            <img
-              src=""
-              className="card-img-top"
-              alt="Image Not Found"
-            ></img>
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                Watch
-              </a>
-            </div>
-          </div>
-        </div>
+          )}
       </div>
-      {/* <div className="float-center fs-1">Sorry We have Nothing to show :(</div> */}
     </>
   );
 };
